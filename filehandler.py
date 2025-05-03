2efd91706f85dda326b45913dbbee5105752fd44
svdlh <dlugosch3@gmail.com>
2025-05-03 19:46:12 +0200

    filehandler for imput/output situations

---
+++ filehandler.py
@@ -0,0 +1,13 @@
+import os
+
+#filepaths
+output_qza = "output/artifact_results"
+output_qzv = "output/emp_results"
+output_ordin = "output/ordination_files"
+
+
+#create directories, exist_ok=True so it wont cry if the directories already exist
+def makefolder():
+    os.makedirs(output_qza, exist_ok=True)
+    os.makedirs(output_qzv, exist_ok=True)
+    os.makedirs(output_ordin, exist_ok=True)
